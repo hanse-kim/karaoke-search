f1805520aa36d0ad012c52aa108212818bb34e8e
bluewood <bluewood.truth@gmail.com>
2022-01-02 22:58:26 +0900

    feat: add apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,35 @@
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import useTheme from 'hooks/useTheme';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import {RecoilRoot} from 'recoil';
 import {ThemeProvider} from 'styled-components';
 import GlobalStyle from 'styles/GlobalStyle';
+
+const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({Component, pageProps}: AppProps) {
   const {theme} = useTheme();
 
   return (
+    <ApolloProvider client={client}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
+          <Head>
+            <title>노래찾기</title>
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1'
+            />
+          </Head>
           <GlobalStyle />
           <Component {...pageProps} />
         </ThemeProvider>
       </RecoilRoot>
+    </ApolloProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
-import axios from 'axios';
-import Head from 'next/head';
+import {Box} from 'components/_common';
+import {Header} from 'views/header';
 
 const Home = () => {
-  axios.post(
-    '/api/graphql',
-    {query: 'query {links {id, title}}'},
-    {headers: {'Content-Type': 'application/json', Accept: 'application/json'}}
-  ).then(item => {console.log(item.data);});
-
   return (
     <div className='container'>
-      <Head>
-        <title>노래찾기</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <main>hello world!</main>
-      <footer></footer>
+      <Header />
+      <Box>This is Home!</Box>
     </div>
   );
 };
