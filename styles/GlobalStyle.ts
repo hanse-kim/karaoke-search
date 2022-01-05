342cf48a7264d8d89177dd26f30428dd0469d60a
bluewood <bluewood.truth@gmail.com>
2022-01-05 11:14:00 +0900

    refactor: change GlobalStyle from object to template string

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,47 +1,43 @@
+import {ThemeProps} from 'components/_common/styles';
 import {createGlobalStyle} from 'styled-components';
-import type {Theme} from './theme';
 import {fontSize} from './units';
 
-interface Props {
-  theme: Theme;
-}
-
-const GlobalStyle = createGlobalStyle((props: Props) => ({
-  html: {
-    boxSizing: 'border-box',
-  },
-
-  body: {
-    fontFamily: `'Noto Sans KR', sans-serif`,
-    background: props.theme.color.white,
-    color: props.theme.color.black,
-    fontSize: fontSize.md,
-  },
-
-  '*': {
-    margin: 0,
-    boxSizing: 'border-box',
-  },
-
-  'body, h1, h2, h3, h4, h5, h6, p, ol, ul': {
-    margin: 0,
-    padding: 0,
-    fontWeight: 'normal',
-  },
-
-  'ol, ul': {
-    listStyle: 'none',
-  },
-
-  ':focus': {
-    outline: 0,
-  },
-
-  '.container': {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
+const GlobalStyle = createGlobalStyle<ThemeProps>`
+  html {
+    box-sizing: border-box;
   }
-}));
+
+  body {
+    font-family: AppleSDGothicNeo, 'Noto Sans KR', sans-serif;
+    background: ${(props) => props.theme.color.white};
+    color: ${(props) => props.theme.color.black};
+    font-size: ${fontSize.md};
+  }
+
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+`;
 
 export default GlobalStyle;
