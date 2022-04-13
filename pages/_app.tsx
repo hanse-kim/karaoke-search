06310527bd17529ea236175041cb3a5cd40363bf
bluewood <bluewood.truth@gmail.com>
2022-04-13 20:19:07 +0900

    refactor: remove styled-components theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import type {AppProps} from 'next/app';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import {ThemeProvider} from 'styled-components';
 import {SearchFilterProvider} from 'contexts/SearchFilterContext';
-import useTheme from 'hooks/useTheme';
 import {Layout} from 'components/layout';
 import GlobalStyle from 'styles/GlobalStyle';
 
@@ -14,24 +12,20 @@ const client = new ApolloClient({
 });
 
 function MyApp({Component, pageProps, router}: AppProps) {
-  const {theme} = useTheme();
-
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-        </Head>
-        <GlobalStyle />
-        <React.StrictMode>
-          <SearchFilterProvider searchFilter={router.query}>
-            <Layout isHome={router.pathname === '/'}>
-              <Component {...pageProps} />
-            </Layout>
-          </SearchFilterProvider>
-        </React.StrictMode>
-      </ThemeProvider>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <GlobalStyle />
+      <React.StrictMode>
+        <SearchFilterProvider searchFilter={router.query}>
+          <Layout isHome={router.pathname === '/'}>
+            <Component {...pageProps} />
+          </Layout>
+        </SearchFilterProvider>
+      </React.StrictMode>
     </ApolloProvider>
   );
 }
