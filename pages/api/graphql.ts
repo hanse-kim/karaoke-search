33d3761d30f8b711006b469f7ebda8e2bedb1944
Hanse Kim <hansekim.dev@gmail.com>
2024-04-14 20:34:09 +0900

    refactor: refactor graphql api

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,25 @@
-import requestHandler from 'server';
+import {NextApiRequest, NextApiResponse} from 'next';
+import apolloServer from 'server';
 
-export default requestHandler;
+const PATH = '/api/graphql';
+
+type RequestHandler = ReturnType<typeof apolloServer.createHandler>;
+
+let apolloRequestHandler: RequestHandler | null = null;
+
+const requestHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (apolloRequestHandler === null) {
+    await apolloServer.start();
+    apolloRequestHandler = apolloServer.createHandler({path: PATH});
+  }
+
+  return await apolloRequestHandler(req, res);
+};
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+export default requestHandler;
--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,6 @@
 import {ApolloServer} from 'apollo-server-micro';
-import {NextApiRequest, NextApiResponse} from 'next';
 import {resolvers, typeDefs} from './modules';
 
-type RequestHandler = (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => Promise<void>;
-
-const PATH = '/api/graphql';
-
 const apolloServer = new ApolloServer({typeDefs, resolvers});
-const serverStart = apolloServer.start();
-let apolloRequestHandler: RequestHandler | null = null;
 
-const requestHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await serverStart;
-  if (apolloRequestHandler === null) {
-    apolloRequestHandler = apolloServer.createHandler({path: PATH});
-  }
-
-  return await apolloRequestHandler(req, res);
-};
-
-export default requestHandler;
+export default apolloServer;
